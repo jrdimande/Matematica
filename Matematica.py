b2ec1cd23516649eecef765d0fa9c64cd1ab9187
jrdimande <timoteojrdimande@gmail.com>
2024-09-05 11:06:22 +0200

    ...

--- Matematica.py
+++ Matematica.py
@@ -13,10 +13,9 @@ class Math:
         return result
 
     def eq_quadratic(self, a, b, c):
-
         delta = b**2 - 4 * a * c
-        x1 = -b + delta ** 1 / 2 / 2 * a
-        x2 = -b - delta ** 1 / 2 / 2 * a
+        x1 = (-b + (delta ** 0.5)) / (2 * a)
+        x2 = (-b - (delta ** 0.5)) / (2 * a)
         response = f"As raízes são: x1 = {x1} ^ x2 = {x2}"
         return response
 
