9eac4a299fc6f57817534a5955dfbd4979860172
jrdimande <timoteojrdimande@gmail.com>
2024-09-05 11:11:06 +0200

    ...

--- Matematica.py
+++ Matematica.py
@@ -13,11 +13,22 @@ class Math:
         return result
 
     def eq_quadratic(self, a, b, c):
-        delta = b**2 - 4 * a * c
-        x1 = (-b + (delta ** 0.5)) / (2 * a)
-        x2 = (-b - (delta ** 0.5)) / (2 * a)
-        response = f"As raízes são: x1 = {x1} ^ x2 = {x2}"
-        return response
+
+        if a == 0:
+            print("a can not be zero")
+
+        elif a != 0:
+            delta = b**2 - 4 * a * c
+
+            if delta < 0:
+                print("This equation doesnt have solution")
+
+            else:
+                x1 = (-b + (delta ** 0.5)) / (2 * a)
+                x2 = (-b - (delta ** 0.5)) / (2 * a)
+                response = f"As raízes são: x1 = {x1} ^ x2 = {x2}"
+
+                return response
 
     def eq_linear(self, a, b):
        try:
@@ -26,4 +37,5 @@ class Math:
 
        except ZeroDivisionError as e:
            print(f"Division by Zero\nCan not be {a}")
+
 
